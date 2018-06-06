07dfb3baf799951e04cff07eb38d791b10cdac8c
JoaoPeterli <janjo_pes@hotmail.com>
2018-06-06 09:47:31 -0300

    Add CSS to the Posts new.html.erb template

--- app/services/posts_for_branch_service.rb
+++ app/services/posts_for_branch_service.rb
@@ -1,4 +1,5 @@
-class PostsForBranchService
+class  PostsForBranchService
+
   def initialize(params)
     @search = params[:search]
     @category = params[:category]
@@ -6,17 +7,18 @@ class PostsForBranchService
   end
 
   # get posts depending on the request
-   def call
-     if @category.blank? && @search.blank?
-       posts = Post.by_branch(@branch).all
-     elsif @category.blank? && @search.present?
-       posts = Post.by_branch(@branch).search(@search)
-     elsif @category.present? && @search.blank?
-       posts = Post.by_category(@branch, @category)
-     elsif @category.present? && @search.present?
-       posts = Post.by_category(@branch, @category).search(@search)
-     else
-     end
+  def call
+    if @category.blank? && @search.blank?
+      posts = Post.by_branch(@branch).all
+    elsif @category.blank? && @search.present?
+      posts = Post.by_branch(@branch).search(@search)
+    elsif @category.present? && @search.blank?
+      posts = Post.by_category(@branch, @category)
+    elsif @category.present? && @search.present?
+      posts = Post.by_category(@branch, @category).search(@search)
+    else
+    end
   end
 
 end
+
